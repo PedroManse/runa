41f5a1510d40a7ebe8fbdc84d1deaa7e8b5fd1e6
Manse <122305437+PedroManse@users.noreply.github.com>
2025-11-06 17:01:58 -0300

    feat(oneshot): pooled manager

--- src/oneshot.rs
+++ src/oneshot.rs
@@ -1,24 +1,22 @@
-use std::any::Any;
-use std::fmt::Debug;
-use std::sync::mpsc;
+use std::marker::PhantomData;
 use std::thread::JoinHandle;
 
-use crate::{ActionResult, CmdRst, Command, CommandRunner};
+use crate::{ActionResult, ChanRecv, CmdRst, Command};
 
-type InternalCommandLink<Cmd> = oneshot::Sender<CmdRst<Cmd>>;
-type ExternalCommandLink<Cmd> = oneshot::Receiver<CmdRst<Cmd>>;
+pub(crate) type InternalCommandLink<Cmd> = oneshot::Sender<CmdRst<Cmd>>;
+pub(crate) type ExternalCommandLink<Cmd> = oneshot::Receiver<CmdRst<Cmd>>;
 
 pub struct QueuedCommand<Cmd>
 where
     Cmd: Command,
 {
-    cmd: Cmd,
-    chan: InternalCommandLink<Cmd>,
+    pub(crate) cmd: Cmd,
+    pub(crate) chan: InternalCommandLink<Cmd>,
 }
 
-impl<Cmd> Debug for QueuedCommand<Cmd>
+impl<Cmd> std::fmt::Debug for QueuedCommand<Cmd>
 where
-    Cmd: Debug + Command,
+    Cmd: std::fmt::Debug + Command,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Queued Command ({:?})", self.cmd)
@@ -26,79 +24,37 @@ where
 }
 
 #[derive(Debug)]
-pub struct OneShotRunner<Cmd>
+pub struct OneShotRunner<Cmd, R>
 where
     Cmd: Command,
+    R: ChanRecv<QueuedCommand<Cmd>>,
 {
-    reqs: mpsc::Receiver<QueuedCommand<Cmd>>,
+    d: PhantomData<Cmd>,
+    pub(crate) reqs: R,
 }
 
-pub struct OneShotAPI<Cmd>
+impl<Cmd, R> OneShotRunner<Cmd, R>
 where
     Cmd: Command,
+    R: ChanRecv<QueuedCommand<Cmd>> + Send + 'static,
+    <R as ChanRecv<QueuedCommand<Cmd>>>::Err: std::fmt::Debug,
 {
-    cmd_queue: mpsc::Sender<QueuedCommand<Cmd>>,
-    thread: JoinHandle<OneShotRunner<Cmd>>,
-}
-
-impl<Cmd> OneShotRunner<Cmd>
-where
-    Cmd: Command,
-    <Cmd as Command>::Result: Debug,
-{
-    fn get(&self) -> Result<QueuedCommand<Cmd>, mpsc::RecvError> {
-        self.reqs.recv()
+    fn get(&self) -> Result<QueuedCommand<Cmd>, R::Err> {
+        self.reqs.recv_t()
     }
     fn exec(cmd: Cmd) -> ActionResult<Cmd::Result> {
         cmd.execute()
     }
-    fn spawn(rx: mpsc::Receiver<QueuedCommand<Cmd>>) -> JoinHandle<Self> {
+    pub(crate) fn spawn(rx: R) -> JoinHandle<Self> {
         std::thread::spawn(move || {
-            let runner = OneShotRunner::<Cmd> { reqs: rx };
+            let runner = Self { reqs: rx, d: PhantomData };
             loop {
                 let msg = runner.get().unwrap();
-                let r = OneShotRunner::exec(msg.cmd);
+                let r = Self::exec(msg.cmd);
                 let ActionResult::Normal(res) = r else { break };
                 msg.chan.send(res).unwrap();
             }
             runner
         })
-    }
-}
-
-#[derive(Debug)]
-pub enum OneShotCloseError<Cmd>
-where
-    Cmd: Command,
-{
-    Send(QueuedCommand<Cmd>),
-    Join(Box<dyn Any + Send>),
-}
-
-impl<Cmd> CommandRunner for OneShotAPI<Cmd>
-where
-    Cmd: Command,
-    <Cmd as Command>::Result: Debug,
-{
-    type Cmd = Cmd;
-    type SendAck = Result<ExternalCommandLink<Cmd>, QueuedCommand<Cmd>>;
-    type CloseResult = Result<OneShotRunner<Cmd>, OneShotCloseError<Cmd>>;
-    fn new() -> Self {
-        let (tx, rx) = std::sync::mpsc::channel();
-        let thread = OneShotRunner::spawn(rx);
-        OneShotAPI {
-            cmd_queue: tx,
-            thread,
-        }
-    }
-    fn send(&self, cmd: Self::Cmd) -> Self::SendAck {
-        let (tx, rx) = oneshot::channel();
-        let msg = QueuedCommand { cmd, chan: tx };
-        self.cmd_queue.send(msg).map_err(|e| e.0)?;
-        Ok(rx)
-    }
-    fn close_with(self, c: impl crate::StopRunner<Self::Cmd>) -> Self::CloseResult {
-        self.send(c.get()).map_err(OneShotCloseError::Send)?;
-        self.thread.join().map_err(OneShotCloseError::Join)
     }
 }
---
+++ src/oneshot_single.rs
@@ -0,0 +1,54 @@
+use std::any::Any;
+use std::fmt::Debug;
+use std::sync::mpsc;
+use std::thread::JoinHandle;
+
+use crate::oneshot::{ExternalCommandLink, OneShotRunner, QueuedCommand};
+use crate::{Command, CommandRunner};
+type SR<Cmd> = mpsc::Receiver<QueuedCommand<Cmd>>;
+
+pub struct OneShotAPI<Cmd>
+where
+    Cmd: Command,
+{
+    cmd_queue: mpsc::Sender<QueuedCommand<Cmd>>,
+    thread: JoinHandle<OneShotRunner<Cmd, SR<Cmd>>>,
+}
+
+#[derive(Debug)]
+pub enum OneShotCloseError<Cmd>
+where
+    Cmd: Command,
+{
+    Send(QueuedCommand<Cmd>),
+    Join(Box<dyn Any + Send>),
+}
+
+impl<Cmd> CommandRunner for OneShotAPI<Cmd>
+where
+    Cmd: Command,
+    <Cmd as Command>::Result: Debug,
+{
+    type Cmd = Cmd;
+    type SendAck = Result<ExternalCommandLink<Cmd>, QueuedCommand<Cmd>>;
+    type CloseResult = Result<OneShotRunner<Cmd, SR<Cmd>>, OneShotCloseError<Cmd>>;
+    fn new() -> Self {
+        let (tx, rx) = std::sync::mpsc::channel();
+        let thread = OneShotRunner::spawn(rx);
+        OneShotAPI {
+            cmd_queue: tx,
+            thread,
+        }
+    }
+    fn send(&self, cmd: Self::Cmd) -> Self::SendAck {
+        let (tx, rx) = oneshot::channel();
+        let msg = QueuedCommand { cmd, chan: tx };
+        self.cmd_queue.send(msg).map_err(|e| e.0)?;
+        Ok(rx)
+    }
+    fn close_with(self, c: impl crate::StopRunner<Self::Cmd>) -> Self::CloseResult {
+        self.send(c.get()).map_err(OneShotCloseError::Send)?;
+        self.thread.join().map_err(OneShotCloseError::Join)
+    }
+}
+
