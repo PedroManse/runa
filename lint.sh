fe00233fe2a61be8a5424da9f1b6e09b97ffeff9
Manse <122305437+PedroManse@users.noreply.github.com>
2025-11-21 15:33:58 -0300

    ci(lint): simplify clippy usage

--- lint.sh
+++ lint.sh
@@ -9,25 +9,10 @@ if [ -n "$DIRTY" ] && [ "$DIRTY" != "0" ] ; then
 	allow_dirty="--allow-dirty"
 fi
 
+set -x
 
-ci() {
-	pushd "$1"
-	set -x
-
-	cargo clippy $fix $allow_dirty --all-targets --all-features -- \
-		-Dclippy::perf \
-		-Dclippy::style \
-		-Dclippy::pedantic \
-		-Wclippy::missing_errors_doc
-
-	set +x
-	popd
-}
-
-if [ "$#" != 0 ] ; then
-	for target in "$@" ; do
-		ci "$target"
-	done
-else
-	ci .
-fi
+cargo clippy $fix $allow_dirty --all-targets --all-features -- \
+	-Dclippy::perf \
+	-Dclippy::style \
+	-Dclippy::pedantic \
+	-Wclippy::missing_errors_doc
