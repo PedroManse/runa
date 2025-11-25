89023c7e248ac3af50a0097698e56e279db47ddb
Manse <122305437+PedroManse@users.noreply.github.com>
2025-11-25 15:57:45 -0300

    ci(clippy): use cargo.toml to setup clippy lint levels

--- ci.sh
+++ ci.sh
@@ -1,11 +1,16 @@
 #! /usr/bin/env bash
 
 set -ex
+if [ -n "$FIX" ] && [ "$FIX" != "0" ] ; then
+	fix="--fix"
+fi
+
+if [ -n "$DIRTY" ] && [ "$DIRTY" != "0" ] ; then
+	allow_dirty="--allow-dirty"
+fi
+
 cargo fmt
 cargo check
 cargo build
 cargo test
-
-set +x
-bash lint.sh $@
-set +e
+cargo clippy $fix $allow_dirty --all-targets --all-features
